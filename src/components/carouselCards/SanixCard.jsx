d83d38e9cca91e140f42fa247e573387c9105073
Track404 <111713000+Track404@users.noreply.github.com>
2025-04-06 21:44:41 +0200

    creation of the SanixCard

---
+++ src/components/carouselCards/SanixCard.jsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { useTranslation } from 'react-i18next';
+import { Safari } from '@/components/magicui/safari';
+import Iphone15Pro from '@/components/magicui/iphone-15-pro';
+
+export default function SanixCard() {
+  const { t } = useTranslation();
+
+  const features = t('projects.sanix.card.sanix.features', {
+    returnObjects: true,
+  });
+  const technologies = t('projects.sanix.card.sanix.technologies', {
+    returnObjects: true,
+  });
+
+  return (
+    <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+      <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-lg">
+        {t('projects.sanix.card.sanix.description')}
+      </p>
+
+      <h2 className="text-2xl font-bold mt-6 text-neutral-700 dark:text-neutral-200">
+        {t('projects.sanix.card.sanix.featuresTitle')}
+      </h2>
+      <ul className="list-disc list-inside text-neutral-600 dark:text-neutral-400">
+        {features.map((feature, index) => (
+          <li key={index} dangerouslySetInnerHTML={{ __html: feature }} />
+        ))}
+      </ul>
+
+      <h2 className="text-2xl font-bold mt-6 text-neutral-700 dark:text-neutral-200">
+        {t('projects.sanix.card.sanix.demoTitle')}
+      </h2>
+
+      <a
+        href="https://shoppingcartsanix.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block mt-6 mb-6 px-8 py-4 text-white text-xl font-semibold rounded-xl shadow 
+           bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3]
+           transform transition-transform duration-300 ease-in-out
+           hover:scale-105 active:scale-95"
+      >
+        {t('projects.sanix.card.sanix.liveDemo')}
+      </a>
+
+      <p>
+        🔗{' '}
+        <a
+          href="https://github.com/ClementZambon/sanix"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block pl-1 bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] 
+             bg-clip-text text-transparent font-semibold hover:underline 
+             transform transition-transform duration-300 ease-in-out 
+             hover:scale-105 active:scale-95"
+        >
+          {t('projects.sanix.card.sanix.frontendRepo')}
+        </a>
+      </p>
+
+      <p>
+        🔗{' '}
+        <a
+          href="https://github.com/ClementZambon/sanix-backend"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block pl-1 bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] 
+             bg-clip-text text-transparent font-semibold hover:underline 
+             transform transition-transform duration-300 ease-in-out 
+             hover:scale-105 active:scale-95"
+        >
+          {t('projects.sanix.card.sanix.backendRepo')}
+        </a>
+      </p>
+
+      <Safari
+        imageSrc="/src/assets/sanix-desktop.png"
+        className="w-full h-full mt-5 mb-5"
+      />
+      <Iphone15Pro
+        src="/src/assets/sanix-mobile.png"
+        className="mx-auto w-1/2 h-full mt-5 mb-5"
+      />
+
+      <h2 className="text-2xl font-bold mt-6 text-neutral-700 dark:text-neutral-200">
+        {t('projects.sanix.card.sanix.technologiesTitle')}
+      </h2>
+      <ul className="list-disc list-inside text-neutral-600 dark:text-neutral-400">
+        {technologies.map((tech, index) => (
+          <li key={index}>{tech}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/pages/ProjectsSection.jsx
+++ src/pages/ProjectsSection.jsx
@@ -7,11 +7,10 @@ import Galaxia from '../assets/social.svg';
 import theHive from '../assets/thehive2.svg';
 import Sanix from '../assets/sanix.svg';
 import { useTranslation } from 'react-i18next';
-import { Safari } from '@/components/magicui/safari';
-import Iphone15Pro from '@/components/magicui/iphone-15-pro';
 import GalaxiaCard from '@/components/carouselCards/GalaxiaCard';
 import TheHiveCard from '@/components/carouselCards/TheHiveCard';
 import WheresWaldoCard from '@/components/carouselCards/WaldoCard';
+import SanixCard from '@/components/carouselCards/SanixCard';
 export default function ProjectsSection({ projectsRef, contactRef }) {
   const { t } = useTranslation();
   const cards = data.map((card, index) => (
@@ -38,96 +37,6 @@ export default function ProjectsSection({ projectsRef, contactRef }) {
     </>
   );
 }
-
-const DummyContent = () => {
-  return (
-    <>
-      <div className=" bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
-        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-lg">
-          A modern social media platform designed to showcase my web development
-          skills.
-        </p>
-
-        <h2 className="text-2xl font-bold mt-6 text-neutral-700 dark:text-neutral-200">
-          YO
-        </h2>
-        <ul className="list-disc list-inside text-neutral-600 dark:text-neutral-400">
-          <li>✅ User authentication (JWT & GitHub OAuth2)</li>
-          <li>✅ Create and edit posts</li>
-          <li>✅ Like and comment on posts</li>
-          <li>✅ Follow/unfollow users</li>
-          <li>✅ Secure REST API for data management</li>
-          <li>✅ Responsive design with TailwindCSS</li>
-        </ul>
-
-        <h2 className="text-2xl font-bold mt-6 text-neutral-700 dark:text-neutral-200">
-          Demo
-        </h2>
-
-        <a
-          href="https://galaxiasocial.netlify.app/login"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block mt-6 mb-6 px-8 py-4 text-white text-xl font-semibold rounded-xl shadow 
-             bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3]
-             transform transition-transform duration-300 ease-in-out
-             hover:scale-105 active:scale-95"
-        >
-          🚀 Live Demo
-        </a>
-
-        <p>
-          🔗{' '}
-          <a
-            href="https://github.com/Track404/Galaxia-social-media"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block pl-1 bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] 
-               bg-clip-text text-transparent font-semibold hover:underline 
-               transform transition-transform duration-300 ease-in-out 
-               hover:scale-105 active:scale-95"
-          >
-            FrontEnd Repo
-          </a>
-        </p>
-
-        <p>
-          🔗{' '}
-          <a
-            href="https://github.com/Track404/Galaxia-social-media-backend"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block pl-1  bg-gradient-to-r from-[#FF0080] via-[#7928CA] to-[#0070F3] 
-               bg-clip-text text-transparent font-semibold hover:underline 
-               transform transition-transform duration-300 ease-in-out 
-               hover:scale-105 active:scale-95"
-          >
-            Backend Repo
-          </a>
-        </p>
-
-        <Safari
-          imageSrc="/src/assets/screenshot.png"
-          className="w-full h-full mt-5 mb-5"
-        />
-        <Iphone15Pro
-          src="/src/assets/screenshotMobile.png"
-          className="mx-auto w-1/2  h-full mt-5 mb-5 "
-        />
-        <h2 className="text-2xl font-bold mt-6 text-neutral-700 dark:text-neutral-200">
-          Technologies
-        </h2>
-        <ul className="list-disc list-inside text-neutral-600 dark:text-neutral-400">
-          <li>🛠 Frontend: React, TailwindCSS</li>
-          <li>🚀 Backend: Node.js, Express.js</li>
-          <li>🛢 Database: PostgreSQL</li>
-          <li>🔐 Authentication: JWT, OAuth2 (GitHub)</li>
-          <li>📡 API Type: REST API</li>
-        </ul>
-      </div>
-    </>
-  );
-};
 
 const data = [
   {
@@ -159,7 +68,7 @@ const data = [
     category: 'Sanix',
     title: 'Shopping Site',
     src: Sanix,
-    content: <DummyContent />,
+    content: <SanixCard />,
     bgColor: 'bg-blue-300',
   },
 ];
